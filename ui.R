f1c077340b55287de61b71280fddc5780089a94f
cicarrascog <carlos.carrasco@anu.edu.au>
2023-11-15 17:20:32 +1100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,12 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(bslib)
 library(pins)
@@ -14,7 +5,6 @@ library(dplyr)
 library(stringr)
 library(rpart)
 library(reactable)
-# library(readr)
 library(tidymodels)
 
 
